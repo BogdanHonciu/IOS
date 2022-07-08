259287ee46f0e912d31b57b61fb903d326c51a6b
Ivi123 <ivona_racasean9@yahoo.com>
2022-07-08 20:25:39 +0200

    first commit

---
+++ proiect ios honciu bogdan 354/ToDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  proiect ios honciu bogdan 354
+//
+//  Created by user217572 on 7/8/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
---
+++ proiect ios honciu bogdan 354/ToDoListItem/ToDoListItem+CoreDataProperties.swift
@@ -0,0 +1,26 @@
+//
+//  ToDoListItem+CoreDataProperties.swift
+//  proiect ios honciu bogdan 354
+//
+//  Created by user217572 on 7/8/22.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension ToDoListItem {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<ToDoListItem> {
+        return NSFetchRequest<ToDoListItem>(entityName: "ToDoListItem")
+    }
+
+    @NSManaged public var name: String?
+    @NSManaged public var createdAt: Date?
+
+}
+
+extension ToDoListItem : Identifiable {
+
+}
